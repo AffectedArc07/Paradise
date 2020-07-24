f4274eb28887e30094d6f7c865743917459247ed
AffectedArc07 <josephcase55@hotmail.com>
2020-07-24 13:42:34 +0100

    hm

--- tools/github-actions/ci/install_phpenv.sh
+++ tools/github-actions/ci/install_phpenv.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+source _build_dependencies.sh
+
 # Script to install PHPEnv for GitHub actions
 curl -L http://git.io/phpenv-installer | sudo bash
+
+# Install the right version of php
+sudo apt install php$PHP_VERSION
