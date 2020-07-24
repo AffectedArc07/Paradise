c809c91ca83204345d714050ca2247ae3280bc43
AffectedArc07 <josephcase55@hotmail.com>
2020-07-24 13:38:49 +0100

    I didnt want to have to do this

--- tools/github-actions/ci/install_phpenv.sh
+++ tools/github-actions/ci/install_phpenv.sh
@@ -3,9 +3,3 @@ set -euo pipefail
 
 # Script to install PHPEnv for GitHub actions
 curl -L http://git.io/phpenv-installer | sudo bash
-# Add it to path
-export PHPENV_ROOT="/home/runner/.phpenv"
-if [ -d "${PHPENV_ROOT}" ]; then
-  export PATH="${PHPENV_ROOT}/bin:${PATH}"
-  eval "$(phpenv init -)"
-fi
