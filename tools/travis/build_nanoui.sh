3448eb022c5112a9f166d251a7c4e0e55be699fe
AffectedArc07 <josephcase55@hotmail.com>
2020-07-24 12:43:07 +0100

    FORCEEEEEEEEEEEEEEEEE

--- tools/travis/build_nanoui.sh
+++ tools/travis/build_nanoui.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 cd nano
 source ~/.nvm/nvm.sh
 sudo npm install -g gulp-cli
-npm install --loglevel=error
+sudo npm install --loglevel=error --force
 node node_modules/gulp/bin/gulp.js --require less-loader
 cd ..
