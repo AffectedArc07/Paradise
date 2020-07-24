bc2360d23c5868d3dc7ad2ecd73c9ba88e3a3a69
AffectedArc07 <josephcase55@hotmail.com>
2020-07-24 13:22:33 +0100

    ...

--- tools/github-actions/ci/install_phpenv.sh
+++ tools/github-actions/ci/install_phpenv.sh
@@ -1,2 +1,5 @@
 # Script to install PHPEnv for GitHub actions
-curl -L http://git.io/phpenv-installer | sudo bash
+git clone git://github.com/phpenv/phpenv.git ~/.phpenv
+echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
+exec $SHELL -l
