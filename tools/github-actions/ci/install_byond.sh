369b1100b3c48b8440fb885d00b2004d3cb8a9cc
AffectedArc07 <josephcase55@hotmail.com>
2020-07-25 15:33:49 +0100

    aaa

--- tools/github-actions/ci/install_byond.sh
+++ tools/github-actions/ci/install_byond.sh
@@ -18,13 +18,7 @@ unzip byond.zip
 rm byond.zip
 cd byond
 make here
-echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
 cd ~/
-
-echo "::set-env name=BYOND_SYSTEM::/home/runner/BYOND/byond"
-echo "::set-env name=PATH::/home/runner/BYOND/byond/bin:$PATH"
-echo "::set-env name=LD_LIBRARY_PATH::/home/runner/BYOND/byond/bin:$LD_LIBRARY_PATH"
-echo "::set-env name=MANPATH::/home/runner/BYOND/byond/man:$MANPATH"
 
 source ~/BYOND/byond/bin/byondsetup
 echo "BYOND Installed"
